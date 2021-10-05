c00eec6188b53ffa38fe871286efd147fdeaa8bb
Audric Rosier <audricrosier@gmail.com>
2021-10-05 13:58:23 +0200

    Create Class user

---
+++ class/user.php
@@ -0,0 +1,50 @@
+<?php
+    class user{
+        private $user_name;
+        private $user_pass;
+        private $user_login;
+        private $user_bday;
+
+        //Function to Consctruct
+        public function __construct($n, $p, $l, $bd){
+            $this -> user_name = $n;
+            $this -> user_pass = $p;
+            $this -> user_login = $l;
+            $this -> user_bday = $bd;
+        }
+
+        //Function to read
+        public function getNom(){
+            return $this -> user_name;
+        }
+
+        public function getPass(){
+            return $this -> user_pass;
+        }
+
+        public function getLogin(){
+            return $this -> user_login;
+        }
+
+        public function getBday(){
+            return $this -> user_bday;
+        }
+
+        //Function to Edit
+        public function setNom($new_user_name){
+            $this -> user_name = $new_user_name;
+        }
+
+        public function setPass($new_user_pass){
+            $this -> user_pass = $new_user_pass;
+        }
+
+        public function setLogin($new_user_login){
+            $this -> user_login = $new_user_login;
+        }
+
+        public function setBday($new_user_bday){
+            $this -> user_bday = $new_user_bday;
+        }
+    }
+?>
