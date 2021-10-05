baa32f8c03501ea3abe172973b0430367551a404
Audric Rosier <audricrosier@gmail.com>
2021-10-05 14:13:32 +0200

    Update user.php

--- class/user.php
+++ class/user.php
@@ -4,13 +4,17 @@
         private $user_pass;
         private $user_login;
         private $user_bday;
+        private $user_pay;
+        private $user_maybeBan;
 
         //Function to Consctruct
-        public function __construct($n, $p, $l, $bd){
+        public function __construct($n, $p, $l, $bd, $pay, $ban){
             $this -> user_name = $n;
             $this -> user_pass = $p;
             $this -> user_login = $l;
             $this -> user_bday = $bd;
+            $this -> user_pay = $pay;
+            $this -> user_maybeBan = $ban;
         }
 
         //Function to read
@@ -30,6 +34,14 @@
             return $this -> user_bday;
         }
 
+        public function getPay(){
+            return $this -> user_bday;
+        }
+
+        public function getBan(){
+            return $this -> user_maybeBan;
+        }
+
         //Function to Edit
         public function setNom($new_user_name){
             $this -> user_name = $new_user_name;
@@ -45,6 +57,14 @@
 
         public function setBday($new_user_bday){
             $this -> user_bday = $new_user_bday;
+        }
+        
+        public function setPay($new_user_pay){
+            $this -> user_pay = $new_user_pay;
+        }
+
+        public function setBan($new_user_ban){
+            $this -> user_maybeBan = $new_user_ban;
         }
     }
 ?>
