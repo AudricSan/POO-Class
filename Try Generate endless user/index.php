6ac656485081bd0d53f1e1de112a8b20a2dcd231
Audric Rosier <audricrosier@gmail.com>
2021-10-05 17:14:59 +0200

    Update index.php

--- Try Generate endless user/index.php
+++ Try Generate endless user/index.php
@@ -1,6 +1,7 @@
 <?php
     //Include Class 
         require 'class/user.php';
+        require 'function/function.php';
     //
 ?>
 
@@ -17,8 +18,27 @@
         <h1> coucou </h1>
 
         <?php
-            $uniqId = uniqid();
+            $uniqId = uniqid("user_");
             var_dump($uniqId);
+
+            $n = RandomString(5);
+            $p = RandomString(10);
+            $l = RandomString(3);
+            $bd = RandomDate();
+            $pay = RandomBool();
+            $sta = $uniqId;
+
+            $uniqId = new user($n, $p, $l, $bd, $pay, $sta);
+            $newUser = $uniqId;
+
+            var_dump($newUser);
+            
+            /*
+            var_dump(RandomBool());
+            var_dump(RandomString(10));
+            var_dump(RandomDate());
+            var_dump(uniqid());
+            */
         ?>
     </body>
 </html>
